bcd191bf26ca6a748eaff062770f8fdcc0f62ca9
Sean <seancapelle@gmail.com>
2024-05-08 12:45:00 -0400

    Render pieces

--- chess-game/src/app/modules/chess-board/chess-board.component.ts
+++ chess-game/src/app/modules/chess-board/chess-board.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { ChessBoard } from '../../chess-logic/chess-board/chess-board';
-import { Color, FENChar } from '../../chess-logic/models';
-import { NgFor } from '@angular/common';
+import { Color, FENChar, pieceImagePaths } from '../../chess-logic/models';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-chess-board',
   standalone: true,
-  imports: [NgFor],
+  imports: [CommonModule, NgFor],
   templateUrl: './chess-board.component.html',
   styleUrl: './chess-board.component.css'
 })
 
 export class ChessBoardComponent {
+  public pieceImagePaths = pieceImagePaths;
+
   private chessBoard = new ChessBoard();
   public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
   public get playerColor(): Color { return this.chessBoard.playerColor; };
+
+  public isSquareDark(x: number, y: number): boolean {
+    return ChessBoard.isSquareDark(x, y);
+  }
 }
